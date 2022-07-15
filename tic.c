8cfc0b70ac59a70de380b3fc52670ae58220793d
Joseph Corbett <jwcorbettin@gmail.com>
2022-07-15 14:08:53 -0400

    Added function print_board that displays the game inside the terminal.

--- tic.c
+++ tic.c
@@ -1,24 +1,40 @@
-#include "tic.h"
+﻿#include "tic.h"
 
 #include <malloc.h>
 #include <stdio.h>
 
 int main() {
-	printf("Starting tic-tac-toe...");
+	printf("Starting tic-tac-toe...\n");
 
 	play_game();
 
-	printf("Thanks for playing!");
+	printf("Thanks for playing!\n");
 }
 
-void clear_board(char* tiles[]) {
+void clear_board(struct tile* tiles[]) {
 
 }
 
 void play_game() {
+	struct tile tiles[9];
+	for (int i = 0; i < (COLUMNS * ROWS); i++) {
+		tiles[i].value = ' ';
+	}
 
+	print_board(tiles);
 }
 
-void print_board(char* tiles[]) {
+void print_board(struct tile tiles[]) {
+	printf("\n");
 
+	for (int i = 0; i < ROWS; i++) {
+		printf(" ");
+		for (int j = 0; j < COLUMNS; j++) {
+			printf(" %c ", tiles[i + (COLUMNS * j)].value);
+			if (j < 2) printf("|");
+		}
+
+		if (i < 2) printf("\n ---+---+--- \n");
+		else printf("\n\n");
+	}
 }
--- tic.h
+++ tic.h
@@ -10,8 +10,9 @@ struct row
 	struct tile* tile_3;
 };
 
-void clear_board(char* []);
-void print_board(char* []);
+void clear_board(struct tile* []);
+void play_game();
+void print_board(struct tile []);
 
 #define ROWS (3)
 #define COLUMNS (3)
